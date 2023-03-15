13e11d894f0844eefd8103f2eed7cf1c07ea751b
Roshan Ranabhat <roshanranabhat11@gmail.com>
2023-03-15 09:00:20 +0545

    registration completed

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Row,
+  Form,
+  Button,
+  Card,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -9,67 +18,122 @@ const Register = () => {
     password: "",
   });
 
+  const [error, setError] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const [alert, setAlert] = useState({
+    show: false,
+    type: "success",
+    message: "",
+  });
+
   const onFormChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
+    setError({
+      ...error,
+      [e.target.id]: "",
+    });
+  };
+
+  const submitform = async (e) => {
+    try {
+      e.preventDefault();
+      await axios({
+        method: "post",
+        url: "http://localhost:4000/api/user",
+        data: formData,
+      });
+      setAlert({
+        show: true,
+        type: "success",
+        message: "Registration successful",
+      });
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      });
+    } catch (err) {
+      setError({
+        ...err.response.data,
+      });
+    }
   };
 
   return (
     <Container>
       <Row>
         <Col>
-          {JSON.stringify(formData)}
-          <Form>
-            <Form.Group className="mb-3" controlId="firstName">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter First Name"
-                onChange={onFormChange}
-              />
-              {/* <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text> */}
-            </Form.Group>
+          <Card>
+            <Card.Title>Registration</Card.Title>
+            <Card.Body>
+              {alert.show ? (
+                <Alert key={alert.type} variant={alert.type}>
+                  {alert.message}
+                </Alert>
+              ) : (
+                ""
+              )}
+              <Form>
+                <Form.Group className="mb-3" controlId="firstName">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={formData.firstName}
+                    placeholder="Enter First Name"
+                    onChange={onFormChange}
+                  />
+                  <Form.Text className="text-muted">
+                    {error.firstName}
+                  </Form.Text>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="lastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Last Name"
-                onChange={onFormChange}
-              />
-              {/* <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text> */}
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="lastName">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={formData.lastName}
+                    placeholder="Enter Last Name"
+                    onChange={onFormChange}
+                  />
+                  <Form.Text className="text-muted">{error.lastName}</Form.Text>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                onChange={onFormChange}
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    value={formData.email}
+                    placeholder="Enter email"
+                    onChange={onFormChange}
+                  />
+                  <Form.Text className="text-muted">{error.email}</Form.Text>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={onFormChange}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Register
-            </Button>
-          </Form>
+                <Form.Group className="mb-3" controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={formData.password}
+                    placeholder="Password"
+                    onChange={onFormChange}
+                  />
+                  <Form.Text className="text-muted">{error.password}</Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={submitform}>
+                  Register
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
